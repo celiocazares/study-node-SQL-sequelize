3d9f0b717064f1c4cdbd608b7d395b264e91c66c
Celio <celiocazares99@gmail.com>
2020-10-25 11:07:38 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'asdf1234',
+    database: 'postgres',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+}
